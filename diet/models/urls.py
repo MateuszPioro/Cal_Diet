1705e605eb2ba4e43352dee55abb0990608c734b
MateuszPioro <pioro199@gmail.com>
2023-04-02 23:48:50 +0200

    next

--- diet/models/urls.py
+++ diet/models/urls.py
@@ -15,9 +15,10 @@ urlpatterns = [
     path('diary/',views.diary_list,name='diary_list'),
     path('diary/<int:diary_id>/',views.diary_detail,name='diary_detail'),
     path('diary/<int:diary_id>/remove/<int:product_id>/',views.diary_remove_product,name='diary_remove_product'),
-    path('diary/add_product/<int:diary_id>/',views.diary_add_product,name='diary_add_product'),
+    path('diary/add_product/<int:diary_id>/',views.add_product_to_diary,name='add_product_to_diary'),
     path('add_product/',views.add_product,name="add_product"),
     path('products/<int:product_id>',views.remove_product,name='remove_product'),
     path('product/<int:product_id>',views.update_product,name='update_product'),
-    path('add_diary/',views.add_diary,name="add_diary")
+    path('add_diary/',views.add_diary,name="add_diary"),
+    path('diary/<int:diary_id>/add_product/', views.add_product_to_diary, name='add_product_to_diary'),
 ]
